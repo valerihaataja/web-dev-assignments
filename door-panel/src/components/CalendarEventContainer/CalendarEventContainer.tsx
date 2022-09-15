bcc1da7360ceac3bee2f17f67b67858a6fa63a4a
Valeri Haataja <valerihaataja@Valeri-MacBook-Pro.local>
2022-09-15 16:08:33 +0300

    Populate ui with data

--- door-panel/src/components/CalendarEventContainer/CalendarEventContainer.tsx
+++ door-panel/src/components/CalendarEventContainer/CalendarEventContainer.tsx
@@ -1,11 +1,14 @@
+import Moment from 'react-moment';
 import styles from './CalendarEventContainer.module.css';
 
-const CalendarEventContainer = () => {
+const CalendarEventContainer = ({ name, startTime, endTime }: { name: string, startTime: number, endTime: number }) => {
 
     return (
         <div className={styles.calendarEventContainer}>
-            <h1>Nimi</h1>
-            <h1>10.00-11.00</h1>
+            <h1>{name}</h1>
+            <h2><Moment>{startTime}</Moment></h2>
+            <h2><Moment format='HH:mm'>{startTime}</Moment></h2>
+            <h2><Moment format='HH:mm'>{endTime}</Moment></h2>
         </div>
     )
 }
--- door-panel/src/components/Content/Content.tsx
+++ door-panel/src/components/Content/Content.tsx
@@ -1,36 +1,34 @@
 import CalendarEventContainer from "../CalendarEventContainer/CalendarEventContainer";
 import Header from "../Header/Header";
-import { Link, Navigate, useNavigate, useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import { EbContext } from '../..';
 import { useContext } from 'react';
 import { useState } from "react";
 
 
-
 const Content = () => {
     const eb = useContext(EbContext) as any;
     const { roomId } = useParams();
-    const [event, setEvent] = useState({} as any);
-    const [todaysEvents, updateEvents] = useState([])
-
+    const temp = eb.sorted_assets.by_outline[roomId as any].find((obj: { type: string; }) => obj.type === "temperature").data_provider_most_recent_by_type.temperature.value.value;
+    const co2 = eb.sorted_assets.by_outline[roomId as any].find((obj: { type: string; }) => obj.type === "co2").data_provider_most_recent_by_type.co2.value.value;
+    const [roomTemperature, setTemperature] = useState(temp);
+    const [roomCO2, setCO2] = useState(co2);
     const found = eb.sorted_assets.by_type.room.find((obj: { id: string; }) => {
         return obj.id === roomId;
     });
+    const now = Date.now();
+    const [todaysEvents, updateEvents] = useState<any[]>(found.data_provider_most_recent_by_type.reservation.value.events.length > 0 ? found.data_provider_most_recent_by_type.reservation.value.events
+        .filter((e: { end: number; }) => e.end >= now && Intl.DateTimeFormat('en-US').format(e.end) === Intl.DateTimeFormat('en-US').format(now)) : []);
 
     window.eb_core.eb_add_hook(eb, "post_asset_dp_update", (eb: any, asset: { name: any; }, dp: { type: any; value: any; }) => {
-        // console.log(asset.name);
-        // console.log(dp.type);
-        // console.log(dp.value);
-        // const array = [];
-        // updateEvents([]);
-        // for (let i = 0; i < dp.value.events.lenght; i++) {
-        //     console.log("Pogg");
-        // }
-        console.log("Mita taalla on", dp.value);
-        updateEvents(arr => [])
-        setEvent(dp.value);
+        if (dp.value.events.length > 0) {
+            updateEvents(dp.value.events.filter((e: { end: number; }) => e.end >= now && Intl.DateTimeFormat('en-US').format(e.end) === Intl.DateTimeFormat('en-US').format(now)));
+            setTemperature(temp);
+            setCO2(co2);
+            console.log(temp);
 
-    }, (eb: any, asset: { id: any; }, dp: any) => asset.id == roomId);
+        }
+    }, (eb: any, asset: { id: any; }, dp: any) => asset.id === roomId);
 
     if (found == null)
         return (<div><h1>No room found</h1></div>)
@@ -38,19 +36,19 @@ const Content = () => {
         <div className='Content'>
             <Link to="/">HOME</Link>
             <Header />
-            <h1>{found.name != undefined ? found.name : "ss"}</h1>
+            <h1>{found.name !== undefined ? found.name : "ss"}</h1>
             <h2>A meeting room for 10 people with 1 persion inside</h2>
-            <h2>{event.time}</h2>
-            <CalendarEventContainer />
-            <CalendarEventContainer />
+
+            {todaysEvents.length > 0 ? todaysEvents.map(({ subject, start, end }: any) => {
+                return <CalendarEventContainer key={start + end} name={subject} startTime={start} endTime={end} />
+            }) : <h3>No reservations</h3>}
+
             <div className='row'>
-                <p className='text'>Temperature: 21.4</p>
-                <p className='text'>CO2: 444</p>
+                <p className='text'>Temperature: {roomTemperature}</p>
+                <p className='text'>CO2: {roomCO2}</p>
             </div>
         </div>
     )
 }
 
 export default Content;
-//112038283
-//112038086     225/50R17 premium n 900e
