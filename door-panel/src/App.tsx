87e9bfdd764ea19f15ba2712779ac706b18ad35c
Valeri Haataja <valerihaataja@Valeri-MacBook-Pro.local>
2022-09-19 19:36:01 +0300

    Clean up code

--- door-panel/src/App.tsx
+++ door-panel/src/App.tsx
@@ -1,39 +1,13 @@
-import React, { useContext } from 'react';
-import { Outlet, Link, useNavigate } from "react-router-dom";
 import './App.css';
-import { useState } from "react";
-import { EbContext } from '.';
+import InputField from './components/InputField/InputField';
 
 
 function App() {
-  const eb = useContext(EbContext) as any;
-  const [filterText, setFilterText] = useState("");
-  let navigate = useNavigate();
-  const routeChange = () => {
-    let isRoom = eb.sorted_assets.by_type.room.find((obj: { id: string; }) => obj.id === filterText);
-    let path = `room/${filterText}`;
-    if (isRoom) {
-      navigate(path);
-    } else {
-      navigate("notfound");
-    }
-    setFilterText("");
-  }
   return (
     <div className='Root'>
       <h1>Door panel homepage</h1>
       <h3>Enter room ID to continue</h3>
-      <nav>
-
-        <input
-          value={filterText}
-          onChange={(event) => {
-            setFilterText(event.target.value);
-          }}
-        />
-        <button onClick={routeChange}>Search</button>
-      </nav>
-      <Outlet />
+      <InputField />
     </div>
   );
 
